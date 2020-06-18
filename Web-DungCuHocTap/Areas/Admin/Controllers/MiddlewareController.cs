64cb29d192622c3daec254a6658b1f5f48a0d132
hoangthai99 <hoangtv130999@gmail.com>
2020-06-18 19:58:34 +0700

    admin-add-sanpham

--- Web-DungCuHocTap/Areas/Admin/Controllers/MiddlewareController.cs
+++ Web-DungCuHocTap/Areas/Admin/Controllers/MiddlewareController.cs
@@ -9,17 +9,17 @@ namespace Web_DungCuHocTap.Areas.Admin.Controllers
 {
     public class MiddlewareController : Controller
     {
-        // GET: Admin/Middleware
+      
         protected override void OnActionExecuting(ActionExecutingContext filterContext)
         {
-            //var session = Session["AdminLogin"] as Web_DungCuHocTap.Models.Models.Admin;
-            //if (session == null)
-            //{
-            //    filterContext.Result = new RedirectToRouteResult(
-            //        new RouteValueDictionary(
-            //            new { controller = "Admin", action = "Login", Area = "Admin" })
-            //        );
-            //}
+            var session = Session["AdminLogin"] as Web_DungCuHocTap.Models.Models.Admin;
+            if (session == null)
+            {
+                filterContext.Result = new RedirectToRouteResult(
+                    new RouteValueDictionary(
+                        new { controller = "Admin", action = "Login", Area = "Admin" })
+                    );
+            }
             base.OnActionExecuting(filterContext);
         }
     }
